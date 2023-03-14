ed63474f6efe2373124ebb941635e1393d982d2d
Cosalpino <61775676+Juillet-Mikael@users.noreply.github.com>
2023-03-14 11:24:23 +0100

    update index

--- view/main.js
+++ view/main.js
@@ -4,7 +4,25 @@ import { GetLocalWeather } from '../controller/requests.js';
 document.querySelector('#app').innerHTML = `
   <input type="text" placeholder="city" value="Lausanne" id="city">
   <button type="button" id="search">search</button>
-  <div id="result"></div>
+  
+  <div>
+    <div>day</div>
+    <div>sun info</div>
+    <div>wind info</div>
+    <div>sunreis</div>
+    <div>sunset</div>
+    <div>daily hours</div>
+    <div>models</div>
+    <div>rain</div>
+    <div>degree</div>
+  </div>
+
+  <div>
+    <div>day</div>
+    <div>model</div>
+    <div>degree</div>
+  </div>
+
 `
 
 const input = document.querySelector('#city');
