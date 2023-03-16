efb9d364a9bbe91ecfe7c642b2b9895b5b58386a
Cosalpino <mikael.juillet@cpnv.ch>
2023-03-16 15:27:19 +0100

    Update view

--- view/main.js
+++ view/main.js
@@ -1,29 +1,46 @@
 import '../assets/css/style.css';
 import { GetLocalWeather } from '../controller/requests.js';
 
+const weather = await GetLocalWeather('Lausanne');
+console.log(weather)
+
 document.querySelector('#app').innerHTML = `
   <input type="text" placeholder="city" value="Lausanne" id="city">
   <button type="button" id="search">search</button>
-  
-  <div>
-    <div>day</div>
-    <div>sun info</div>
-    <div>wind info</div>
-    <div>sunreis</div>
-    <div>sunset</div>
-    <div>daily hours</div>
-    <div>models</div>
-    <div>rain</div>
-    <div>degree</div>
-  </div>
-
-  <div>
-    <div>day</div>
-    <div>model</div>
-    <div>degree</div>
-  </div>
-
+  <div id="weather-container"></div>
 `
+
+const weatherContainer = document.querySelector("#weather-container");
+
+
+for(let i = 0; i < weather.weather.length; i++) {
+  const weatherByDay = weather.weather[i];
+  const weatherDiv = document.createElement("div");
+  if(i == 0){
+    weatherDiv.innerHTML = `
+    <div>${weatherByDay.averageWind}</div>
+    <div>${weatherByDay.chanceOfRain}</div>
+    <div>${weatherByDay.chanceOfThunder}</div>
+    <div>${weatherByDay.day}</div>
+    <div>${weatherByDay.dayTime}</div>
+    <div>${weatherByDay.globalWeather}</div>
+    <div>${weatherByDay.maxTempC}</div>
+    <div>${weatherByDay.minTempC}</div>
+    <div>${weatherByDay.sunHour}</div>
+    <div>${weatherByDay.sunrise}</div>
+    <div>${weatherByDay.sunset}</div>
+    <div>${weatherByDay.totalRain}</div>
+    <div>${weatherByDay.totalSnow}</div>
+    <div>${weatherByDay.windDirection}</div>
+    `;
+  }
+
+    weatherDiv.innerHTML = `
+    <div>${weatherByDay.maxTempC}</div>
+    <div>${weatherByDay.minTempC}</div>
+    `;
+    weatherContainer.appendChild(weatherDiv);
+  }
 
 const input = document.querySelector('#city');
 const button = document.querySelector('#search');
