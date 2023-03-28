ca9d0a9bef2ea004f2db06cade17d00381b42f1d
Cosalpino <mikael.juillet@cpnv.ch>
2023-03-28 08:31:08 +0200

    adding styling to error

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -52,7 +52,7 @@ async function send(){
     <main>
         
         <input type="text" placeholder="city" v-model="weather.city" @keyup.enter="send()">
-        <div v-if="error">
+        <div v-if="error" class="error">
             <h3>{{ error }}</h3>
         </div>
         
@@ -78,5 +78,12 @@ async function send(){
         margin: 20px;
         display: block;
         background: white url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' class='bi bi-search' viewBox='0 0 16 16'%3E%3Cpath d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z'%3E%3C/path%3E%3C/svg%3E") no-repeat 13px center;    
+    }
+
+    .error {
+        display: flex;
+        color: red;
+        align-items: center;
+        justify-content: center;
     }
 </style>
